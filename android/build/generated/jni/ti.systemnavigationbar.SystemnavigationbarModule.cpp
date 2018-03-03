b6119e2223e2a99da736bf0b602a3978d89ed93f
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2018-03-03 08:18:20 +0100

    Init

---
+++ android/build/generated/jni/ti.systemnavigationbar.SystemnavigationbarModule.cpp
@@ -0,0 +1,322 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2017 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "ti.systemnavigationbar.SystemnavigationbarModule.h"
+
+#include "AndroidUtil.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+
+#include "ti.systemnavigationbar.ExampleProxy.h"
+
+#include "org.appcelerator.kroll.KrollModule.h"
+
+#define TAG "SystemnavigationbarModule"
+
+using namespace v8;
+
+namespace ti {
+namespace systemnavigationbar {
+
+
+Persistent<FunctionTemplate> SystemnavigationbarModule::proxyTemplate;
+jclass SystemnavigationbarModule::javaClass = NULL;
+
+SystemnavigationbarModule::SystemnavigationbarModule() : titanium::Proxy()
+{
+}
+
+void SystemnavigationbarModule::bindProxy(Local<Object> exports, Local<Context> context)
+{
+	Isolate* isolate = context->GetIsolate();
+
+	Local<FunctionTemplate> pt = getProxyTemplate(isolate);
+
+	v8::TryCatch tryCatch(isolate);
+	Local<Function> constructor;
+	MaybeLocal<Function> maybeConstructor = pt->GetFunction(context);
+	if (!maybeConstructor.ToLocal(&constructor)) {
+		titanium::V8Util::fatalException(isolate, tryCatch);
+		return;
+	}
+
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Systemnavigationbar"); // use symbol over string for efficiency
+	MaybeLocal<Object> maybeInstance = constructor->NewInstance(context);
+	Local<Object> moduleInstance;
+	if (!maybeInstance.ToLocal(&moduleInstance)) {
+		titanium::V8Util::fatalException(isolate, tryCatch);
+		return;
+	}
+	exports->Set(nameSymbol, moduleInstance);
+}
+
+void SystemnavigationbarModule::dispose(Isolate* isolate)
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Reset();
+	}
+
+	titanium::KrollModule::dispose(isolate);
+}
+
+Local<FunctionTemplate> SystemnavigationbarModule::getProxyTemplate(Isolate* isolate)
+{
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate.Get(isolate);
+	}
+
+	LOGD(TAG, "SystemnavigationbarModule::getProxyTemplate()");
+
+	javaClass = titanium::JNIUtil::findClass("ti/systemnavigationbar/SystemnavigationbarModule");
+	EscapableHandleScope scope(isolate);
+
+	// use symbol over string for efficiency
+	Local<String> nameSymbol = NEW_SYMBOL(isolate, "Systemnavigationbar");
+
+	Local<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(isolate,
+		titanium::KrollModule::getProxyTemplate(isolate)
+, javaClass, nameSymbol);
+
+	proxyTemplate.Reset(isolate, t);
+	t->Set(titanium::Proxy::inheritSymbol.Get(isolate),
+		FunctionTemplate::New(isolate, titanium::Proxy::inherit<SystemnavigationbarModule>));
+
+	// Method bindings --------------------------------------------------------
+	titanium::SetProtoMethod(isolate, t, "setStatusBarColor", SystemnavigationbarModule::setStatusBarColor);
+	titanium::SetProtoMethod(isolate, t, "setNavigationBarColor", SystemnavigationbarModule::setNavigationBarColor);
+	titanium::SetProtoMethod(isolate, t, "hideSystemNavigationbar", SystemnavigationbarModule::hideSystemNavigationbar);
+
+	Local<ObjectTemplate> prototypeTemplate = t->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = t->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return scope.Escape(t);
+}
+
+// Methods --------------------------------------------------------------------
+void SystemnavigationbarModule::setStatusBarColor(const FunctionCallbackInfo<Value>& args)
+{
+	LOGD(TAG, "setStatusBarColor()");
+	Isolate* isolate = args.GetIsolate();
+	HandleScope scope(isolate);
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		titanium::JSException::GetJNIEnvironmentError(isolate);
+		return;
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(SystemnavigationbarModule::javaClass, "setStatusBarColor", "(Ljava/lang/String;)V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'setStatusBarColor' with signature '(Ljava/lang/String;)V'";
+			LOGE(TAG, error);
+				titanium::JSException::Error(isolate, error);
+				return;
+		}
+	}
+
+	Local<Object> holder = args.Holder();
+	// If holder isn't the JavaObject wrapper we expect, look up the prototype chain
+	if (!JavaObject::isJavaObject(holder)) {
+		holder = holder->FindInstanceInPrototypeChain(getProxyTemplate(isolate));
+	}
+
+	titanium::Proxy* proxy = NativeObject::Unwrap<titanium::Proxy>(holder);
+
+	if (args.Length() < 1) {
+		char errorStringBuffer[100];
+		sprintf(errorStringBuffer, "setStatusBarColor: Invalid number of arguments. Expected 1 but got %d", args.Length());
+		titanium::JSException::Error(isolate, errorStringBuffer);
+		return;
+	}
+
+	jvalue jArguments[1];
+
+
+
+
+	
+
+	if (!args[0]->IsNull()) {
+		Local<Value> arg_0 = args[0];
+		jArguments[0].l =
+			titanium::TypeConverter::jsValueToJavaString(
+				isolate,
+				env, arg_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+
+	jobject javaProxy = proxy->getJavaObject();
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	proxy->unreferenceJavaObject(javaProxy);
+
+
+
+				env->DeleteLocalRef(jArguments[0].l);
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException(isolate);
+		env->ExceptionClear();
+	}
+
+
+
+
+	args.GetReturnValue().Set(v8::Undefined(isolate));
+
+}
+void SystemnavigationbarModule::setNavigationBarColor(const FunctionCallbackInfo<Value>& args)
+{
+	LOGD(TAG, "setNavigationBarColor()");
+	Isolate* isolate = args.GetIsolate();
+	HandleScope scope(isolate);
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		titanium::JSException::GetJNIEnvironmentError(isolate);
+		return;
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(SystemnavigationbarModule::javaClass, "setNavigationBarColor", "(Ljava/lang/String;)V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'setNavigationBarColor' with signature '(Ljava/lang/String;)V'";
+			LOGE(TAG, error);
+				titanium::JSException::Error(isolate, error);
+				return;
+		}
+	}
+
+	Local<Object> holder = args.Holder();
+	// If holder isn't the JavaObject wrapper we expect, look up the prototype chain
+	if (!JavaObject::isJavaObject(holder)) {
+		holder = holder->FindInstanceInPrototypeChain(getProxyTemplate(isolate));
+	}
+
+	titanium::Proxy* proxy = NativeObject::Unwrap<titanium::Proxy>(holder);
+
+	if (args.Length() < 1) {
+		char errorStringBuffer[100];
+		sprintf(errorStringBuffer, "setNavigationBarColor: Invalid number of arguments. Expected 1 but got %d", args.Length());
+		titanium::JSException::Error(isolate, errorStringBuffer);
+		return;
+	}
+
+	jvalue jArguments[1];
+
+
+
+
+	
+
+	if (!args[0]->IsNull()) {
+		Local<Value> arg_0 = args[0];
+		jArguments[0].l =
+			titanium::TypeConverter::jsValueToJavaString(
+				isolate,
+				env, arg_0);
+	} else {
+		jArguments[0].l = NULL;
+	}
+
+	jobject javaProxy = proxy->getJavaObject();
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	proxy->unreferenceJavaObject(javaProxy);
+
+
+
+				env->DeleteLocalRef(jArguments[0].l);
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException(isolate);
+		env->ExceptionClear();
+	}
+
+
+
+
+	args.GetReturnValue().Set(v8::Undefined(isolate));
+
+}
+void SystemnavigationbarModule::hideSystemNavigationbar(const FunctionCallbackInfo<Value>& args)
+{
+	LOGD(TAG, "hideSystemNavigationbar()");
+	Isolate* isolate = args.GetIsolate();
+	HandleScope scope(isolate);
+
+	JNIEnv *env = titanium::JNIScope::getEnv();
+	if (!env) {
+		titanium::JSException::GetJNIEnvironmentError(isolate);
+		return;
+	}
+	static jmethodID methodID = NULL;
+	if (!methodID) {
+		methodID = env->GetMethodID(SystemnavigationbarModule::javaClass, "hideSystemNavigationbar", "()V");
+		if (!methodID) {
+			const char *error = "Couldn't find proxy method 'hideSystemNavigationbar' with signature '()V'";
+			LOGE(TAG, error);
+				titanium::JSException::Error(isolate, error);
+				return;
+		}
+	}
+
+	Local<Object> holder = args.Holder();
+	// If holder isn't the JavaObject wrapper we expect, look up the prototype chain
+	if (!JavaObject::isJavaObject(holder)) {
+		holder = holder->FindInstanceInPrototypeChain(getProxyTemplate(isolate));
+	}
+
+	titanium::Proxy* proxy = NativeObject::Unwrap<titanium::Proxy>(holder);
+
+	jvalue* jArguments = 0;
+
+	jobject javaProxy = proxy->getJavaObject();
+	env->CallVoidMethodA(javaProxy, methodID, jArguments);
+
+	proxy->unreferenceJavaObject(javaProxy);
+
+
+
+	if (env->ExceptionCheck()) {
+		titanium::JSException::fromJavaException(isolate);
+		env->ExceptionClear();
+	}
+
+
+
+
+	args.GetReturnValue().Set(v8::Undefined(isolate));
+
+}
+
+// Dynamic property accessors -------------------------------------------------
+
+
+} // systemnavigationbar
+} // ti
